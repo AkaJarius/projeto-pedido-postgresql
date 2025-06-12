6fc09e42f1583aafa9af82d2455b0e27cb58604e
Jairo Alberto <jairo.albertto@gmail.com>
2025-06-11 22:23:25 -0300

    Criacao de mais tabelas

--- scripts.sql
+++ scripts.sql
@@ -192,7 +192,74 @@ delete from cliente where idcliente = 16;
 -- 6. Apague a cliente Sandra
 delete from cliente where idcliente = 18;
 
+-- CRIAÇÃO DE MAIS TABELAS PARA CORREÇÕES
 
+create table profissao (
+	idprofissao integer not null,
+	nome varchar (30) not null,
+
+	constraint pk_prf_idprofissao primary key (idprofissao),
+	constraint un_prf_nome unique (nome)
+);
+
+select profissao from cliente;
+
+insert into profissao (idprofissao, nome) values (1, 'Estudante');
+insert into profissao (idprofissao, nome) values (2, 'Engenheiro');
+insert into profissao (idprofissao, nome) values (3, 'Pedreiro');
+insert into profissao (idprofissao, nome) values (4, 'Jornalista');
+insert into profissao (idprofissao, nome) values (5, 'Professor');
+
+select * from profissao;
+
+create table nacionalidade (
+	idnacionalidade integer not null,
+	nome varchar (30) not null,
+
+	constraint pk_ncn_idnacionalidade primary key (idnacionalidade),
+	constraint un_ncn_nome unique (nome)
+);
+
+select nacionalidade from cliente;
+
+insert into nacionalidade (idnacionalidade, nome) values (1, 'Brasileira');
+insert into nacionalidade (idnacionalidade, nome) values (2, 'Italiana');
+insert into nacionalidade (idnacionalidade, nome) values (3, 'Norte-Americana');
+insert into nacionalidade (idnacionalidade, nome) values (4, 'Alemã');
+
+select * from nacionalidade;
+
+create table complemento (
+	idcomplemento integer not null,
+	nome varchar (30) not null,
+
+	constraint pk_cpl_idcomplemento primary key (idcomplemento),
+	constraint un_cpl_nome unique (nome)
+);
+
+select complemento from cliente;
+
+insert into complemento (idcomplemento, nome) values (1, 'Casa');
+insert into complemento (idcomplemento, nome) values (2, 'Apartamento');
+
+select * from complemento;
+
+create table bairro (
+	idbairro integer not null,
+	nome varchar (30) not null,
+
+	constraint pk_brr_idbairro primary key (idbairro),
+	constraint un_brr_nome unique (nome)
+);
+
+select bairro from cliente;
+
+insert into bairro (idbairro, nome) values (1, 'Cidade Nova');
+insert into bairro (idbairro, nome) values (2, 'Centro');
+insert into bairro (idbairro, nome) values (3, 'São Pedro');
+insert into bairro (idbairro, nome) values (4, 'Santa Rosa');
+
+select * from bairro;
 
 
 
