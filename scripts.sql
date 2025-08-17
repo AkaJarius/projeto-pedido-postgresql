efb553a04b6c0bc2ea66759beb4e73161a9dbe11
Akajarius <jairo.albertto@gmail.com>
2025-08-17 20:33:07 -0300

    DOMINIOS

--- scripts.sql
+++ scripts.sql
@@ -1823,7 +1823,6 @@ EXECUTE PROCEDURE pedido_log();
 
 -- Executando e verificando:
 -- Obs: Só conseguimos apagar pedidos que não possuem produtos, por conta das restrições de chave estrangeira.
-
 SELECT * FROM pedido;
 
 -- Pedidos que não possuem produtos ou vinculo com a tabela de pedido_produto:
@@ -1836,6 +1835,129 @@ DELETE FROM pedido WHERE idpedido = 18;
 
 -- Verificando os dados na nova tabela:
 SELECT * FROM pedidos_apagados;
+
+
+-- DOMÍNIOS:
+/*
+Um Domínio no PostgreSQL é um tipo de dado personalizado baseado em um tipo existente, com restrições adicionais que definem regras de validação de valores.
+
+Em outras palavras, é como criar um “tipo de dado próprio” que garante consistência e integridade dos dados sempre que for usado em colunas de tabelas.
+Tipo de dados: https://www.postgresql.org/docs/current/datatype-numeric.html#DATATYPE-INT
+
+Principais características:
+
+- Baseado em um tipo existente (VARCHAR, INT, NUMERIC, etc.).
+- Pode ter restrições (CHECK, NOT NULL).
+- Facilita padronização de regras entre várias tabelas.
+*/
+
+-- Ids
+create domain idcurto as smallint;
+create domain idmedio as integer;
+create domain idlongo as bigint;
+
+-- Caracteres
+create domain sigla as char(3);
+create domain codigo as varchar(10);
+create domain nome_curto as varchar(15);
+create domain nome_medio as varchar(50);
+create domain nome_longo as varchar(70);
+create domain documento as varchar(15);
+create domain tipo as char(1);
+create domain texto as text;
+
+-- Data e hora
+create domain data as date;
+create domain hora as time;
+create domain data_hora timestamp;
+
+-- Numéricos
+create domain moeda as numeric(10,2);
+create domain float_curto as numeric(6,2);
+create domain float_medio as numeric(10,2);
+create domain float_longo as numeric(15,2);
+create domain quantidade as smallint;
+
+-- Exemplo de utilização de domínio numa tabela:
+ALTER TABLE bairro ALTER COLUMN nome TYPE nome_medio;
+
+/*
+VIEWS EXCLUÍDAS E RESTAURADAS:
+drop view cliente_dados
+drop view cliente_profissao
+drop view dados_pedido
+drop view produto_fornecedor
+drop view municipio_uf
+drop view transportadora_uf
+drop view produto_pedido
+*/
+
+-- EXERCÍCIO:
+/* O objetivo deste exercício é alterar os tipos de dados dos atributos de todas as tabelas, considerando os domínios criados anteriormente. 
+Caso julgue necessário, faça a criação de novos domínios. */
+
+alter table bairro_auditoria alter column data_criacao type data_hora;
+
+alter table cliente alter column nome type nome_longo;
+alter table cliente alter column cpf type documento;
+alter table cliente alter column rg type documento;
+alter table cliente alter column data_nascimento type data;
+alter table cliente alter column genero type tipo;
+alter table cliente alter column logradouro type nome_longo;
+alter table cliente alter column numero type nome_curto;
+alter table cliente alter column idprofissao type idmedio;
+alter table cliente alter column idnacionalidade type idmedio;
+alter table cliente alter column idbairro type idmedio;
+alter table cliente alter column idmunicipio type idmedio;
+alter table cliente alter column idcomplemento type idmedio;
+
+alter table complemento alter column nome type nome_medio;
+
+alter table fornecedor alter column nome type nome_medio;
+
+alter table municipio alter column nome type nome_medio;
+alter table municipio alter column iduf type idmedio;
+
+alter table nacionalidade alter column nome type nome_medio;
+
+alter table pedido alter column idpedido type bigint;
+alter table pedido alter column idcliente type idmedio;
+alter table pedido alter column idtransportadora type idmedio;
+alter table pedido alter column idvendedor type idmedio;
+alter table pedido alter column data_pedido type data;
+alter table pedido alter column valor type moeda;
+
+alter table pedido_produto alter column idpedido type idlongo;
+alter table pedido_produto alter column idproduto type idmedio;
+alter table pedido_produto alter column quantidade type quantidade;
+alter table pedido_produto alter column valor_unitario type moeda;
+
+alter table pedidos_apagados alter column idpedido type idlongo;
+alter table pedidos_apagados alter column idcliente type idmedio;
+alter table pedidos_apagados alter column idtransportadora type idmedio;
+alter table pedidos_apagados alter column idvendedor type idmedio;
+alter table pedidos_apagados alter column data_pedido type data;
+alter table pedidos_apagados alter column valor type moeda;
+
+alter table produto alter column idfornecedor type idmedio;
+alter table produto alter column nome type nome_medio;
+alter table produto alter column valor type moeda;
+
+alter table profissao alter column nome type nome_medio;
+
+alter table transportadora alter column idmunicipio type idmedio;
+alter table transportadora alter column nome type nome_medio;
+alter table transportadora alter column logradouro type nome_longo;
+alter table transportadora alter column numero type nome_curto;
+
+alter table uf alter column nome type nome_medio;
+alter table uf alter column sigla type sigla;
+
+alter table vendedor alter column nome type nome_medio;
+
+
+
+
 
 
 
